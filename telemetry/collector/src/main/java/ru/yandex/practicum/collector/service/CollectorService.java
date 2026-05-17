a6041ce670ec1e94d6a59a140b6b9c0efa8db922
zloy-zhake <jk-jdogg@yandex.ru>
2026-05-17 23:51:11 +0500

    Реализовал отправку событий в Kafka

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/CollectorService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/CollectorService.java
@@ -1,15 +1,149 @@
 package ru.yandex.practicum.collector.service;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.collector.model.HubEvent;
-import ru.yandex.practicum.collector.model.SensorEvent;
+import ru.yandex.practicum.collector.model.*;
+import ru.yandex.practicum.kafka.telemetry.event.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CollectorService {
 
+    private final KafkaProducer<String, byte[]> kafkaProducer;
+
+    @Value("${collector.kafka.topics.sensors}")
+    private String sensorsTopic;
+
+    @Value("${collector.kafka.topics.hubs}")
+    private String hubsTopic;
+
     public void collectSensorEvent(SensorEvent event) {
+        Object payload = switch (event.getType()) {
+            case CLIMATE_SENSOR_EVENT -> {
+                ClimateSensorEvent e = (ClimateSensorEvent) event;
+                yield ClimateSensorAvro.newBuilder()
+                        .setTemperatureC(e.getTemperatureC())
+                        .setHumidity(e.getHumidity())
+                        .setCo2Level(e.getCo2Level())
+                        .build();
+            }
+            case LIGHT_SENSOR_EVENT -> {
+                LightSensorEvent e = (LightSensorEvent) event;
+                yield LightSensorAvro.newBuilder()
+                        .setLinkQuality(e.getLinkQuality())
+                        .setLuminosity(e.getLuminosity())
+                        .build();
+            }
+            case MOTION_SENSOR_EVENT -> {
+                MotionSensorEvent e = (MotionSensorEvent) event;
+                yield MotionSensorAvro.newBuilder()
+                        .setLinkQuality(e.getLinkQuality())
+                        .setMotion(e.isMotion())
+                        .setVoltage(e.getVoltage())
+                        .build();
+            }
+            case SWITCH_SENSOR_EVENT -> {
+                SwitchSensorEvent e = (SwitchSensorEvent) event;
+                yield SwitchSensorAvro.newBuilder()
+                        .setState(e.isState())
+                        .build();
+            }
+            case TEMPERATURE_SENSOR_EVENT -> {
+                TemperatureSensorEvent e = (TemperatureSensorEvent) event;
+                yield TemperatureSensorAvro.newBuilder()
+                        .setTemperatureC(e.getTemperatureC())
+                        .setTemperatureF(e.getTemperatureF())
+                        .build();
+            }
+        };
+
+        SensorEventAvro avro = SensorEventAvro.newBuilder()
+                .setId(event.getId())
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setPayload(payload)
+                .build();
+
+        send(sensorsTopic, event.getHubId(), avro);
     }
 
     public void collectHubEvent(HubEvent event) {
+        Object payload = switch (event.getType()) {
+            case DEVICE_ADDED -> {
+                DeviceAddedEvent e = (DeviceAddedEvent) event;
+                yield DeviceAddedEventAvro.newBuilder()
+                        .setId(e.getId())
+                        .setType(DeviceTypeAvro.valueOf(e.getDeviceType().name()))
+                        .build();
+            }
+            case DEVICE_REMOVED -> {
+                DeviceRemovedEvent e = (DeviceRemovedEvent) event;
+                yield DeviceRemovedEventAvro.newBuilder()
+                        .setId(e.getId())
+                        .build();
+            }
+            case SCENARIO_ADDED -> {
+                ScenarioAddedEvent e = (ScenarioAddedEvent) event;
+                List<ScenarioConditionAvro> conditions = e.getConditions().stream()
+                        .map(c -> ScenarioConditionAvro.newBuilder()
+                                .setSensorId(c.getSensorId())
+                                .setType(ConditionTypeAvro.valueOf(c.getType().name()))
+                                .setOperation(ConditionOperationAvro.valueOf(c.getOperation().name()))
+                                .setValue(c.getValue())
+                                .build())
+                        .toList();
+                List<DeviceActionAvro> actions = e.getActions().stream()
+                        .map(a -> DeviceActionAvro.newBuilder()
+                                .setSensorId(a.getSensorId())
+                                .setType(ActionTypeAvro.valueOf(a.getType().name()))
+                                .setValue(a.getValue())
+                                .build())
+                        .toList();
+                yield ScenarioAddedEventAvro.newBuilder()
+                        .setName(e.getName())
+                        .setConditions(conditions)
+                        .setActions(actions)
+                        .build();
+            }
+            case SCENARIO_REMOVED -> {
+                ScenarioRemovedEvent e = (ScenarioRemovedEvent) event;
+                yield ScenarioRemovedEventAvro.newBuilder()
+                        .setName(e.getName())
+                        .build();
+            }
+        };
+
+        HubEventAvro avro = HubEventAvro.newBuilder()
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setPayload(payload)
+                .build();
+
+        send(hubsTopic, event.getHubId(), avro);
+    }
+
+    private <T extends SpecificRecordBase> void send(String topic, String key, T avro) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+            DatumWriter<T> writer = new SpecificDatumWriter<>(avro.getSchema());
+            writer.write(avro, encoder);
+            encoder.flush();
+            kafkaProducer.send(new ProducerRecord<>(topic, key, out.toByteArray()));
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка сериализации Avro", e);
+        }
     }
 }
