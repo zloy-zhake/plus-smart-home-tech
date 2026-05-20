6e15161c1f26fee509efb914e057002a38234037
zloy-zhake <jk-jdogg@yandex.ru>
2026-05-21 03:34:21 +0500

    Добавил SQL-схему и JPA-сущности

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/model/ScenarioCondition.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.analyzer.model;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "scenario_conditions")
+public class ScenarioCondition {
+
+    @EmbeddedId
+    private ScenarioConditionId id;
+
+    @ManyToOne
+    @MapsId("scenarioId")
+    @JoinColumn(name = "scenario_id")
+    private Scenario scenario;
+
+    @ManyToOne
+    @MapsId("sensorId")
+    @JoinColumn(name = "sensor_id")
+    private Sensor sensor;
+
+    @ManyToOne
+    @MapsId("conditionId")
+    @JoinColumn(name = "condition_id")
+    private Condition condition;
+}
